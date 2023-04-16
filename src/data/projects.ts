00f28a77bbe8ee297d45ae7bb7a1100f0b1c86d1
Lucius Lamparter <lamparter@theriversi.de>
2023-04-16 15:14:21 +0100

    🤵 Update Fluid branding description in showcase

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -24,7 +24,7 @@ export const featureCards: FeatureCardData[] = [
 	{
 		title: "Fluid",
 		description:
-			"A usable yet professional programming framework, fit for any developer. Fluid is an easy language to learn targeting at all developers across the world. Due to it's Python and C architecture, it can process normal Python code, heaven for any developer.",
+			"A usable yet professional cross-platform programming framework for desktop, mobile, web and IoT apps. Fluid is an easy language to learn targeting at all developers across the world. Due to it's Python and C architecture, it can process normal Python code, heaven for any developer.",
 		icon: Flash
 	},
 	{

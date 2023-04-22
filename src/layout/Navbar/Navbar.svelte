270ae3fc1b497eb1ccd57910d0db6e35646817fa
Lamparter <lamparter@theriversi.de>
2023-04-22 18:03:40 +0100

    ✨ Update Navbar.svelte

--- src/layout/Navbar/Navbar.svelte
+++ src/layout/Navbar/Navbar.svelte
@@ -1,4 +1,4 @@
-<script src="https://components.esmerelda.tech/Website/Analytics/One.js"></script>
+<!--<script src="https://components.esmerelda.tech/Website/Analytics/One.js"></script>-->
 <script lang="ts">
 	import { navigating, page } from "$app/stores";
 	import type { NavbarItem } from "$data/links";
